7ee59d99f9cb69b33a7db091756c7be43f6157e7
Jozef Hudec <jojohudec27@gmail.com>
2022-08-12 16:51:34 +0200

    Add ability to play again without having to reload

--- js.js
+++ js.js
@@ -20,12 +20,33 @@ Array.from(choiceNodes).forEach(node => {
     node.addEventListener('click', e => chooseChoice(e.target.parentNode.dataset.choice));
 });
 
-const gameOverModal = document.getElementById('game-over-modal')
-const playAgainBtn = document.querySelector('.play-again-btn')
+const computerHPContainer = document.querySelector('.computer-hp');
+const userHPContainer = document.querySelector('.user-hp');
+
+const roundMsgNode = document.querySelector('.round-msg');
+const roundCounterNode = document.querySelector('.round-counter');
+
+const gameOverModal = document.getElementById('game-over-modal');
+const playAgainBtn = document.querySelector('.play-again-btn');
 
 playAgainBtn.addEventListener('click', () => {
-    window.location.href = ''
-})
+    userHP = 5;
+    computerHP = 5;
+    roundCounter = 1;
+    gameOverMessage = '';
+
+    roundMsgNode.innerHTML = 'Please select <strong>1 item</strong> above';
+    roundCounterNode.textContent = 'ROUND 1';
+
+    for (let node of computerHPContainer.children) {
+        node.textContent = '❤️';
+    };
+    for (let node of userHPContainer.children) {
+        node.textContent = '❤️';
+    };
+
+    gameOverModal.style.display = 'none';
+});
 
 let userHP = 5;
 let computerHP = 5;
@@ -39,11 +60,11 @@ function getComputerChoice() {
 function didUserWinRound(userChoice, computerChoice) {
     switch (userChoice) {
         case ROCK:
-            return computerChoice === SCISSORS
+            return computerChoice === SCISSORS;
         case PAPER:
-            return computerChoice === ROCK
+            return computerChoice === ROCK;
         case SCISSORS:
-            return computerChoice === PAPER
+            return computerChoice === PAPER;
     };
 
 
@@ -52,52 +73,47 @@ function didUserWinRound(userChoice, computerChoice) {
 function playRound(userChoice) {
     const computerChoice = getComputerChoice();
 
-    const roundMsgNode = document.querySelector('.round-msg')
-
-    const roundCounterNode = document.querySelector('.round-counter')
-    roundCounterNode.textContent = `ROUND ${++roundCounter}`
+    roundCounterNode.textContent = `ROUND ${++roundCounter}`;
 
     if (computerChoice === userChoice) {
-        roundMsgNode.innerHTML = `The computer chose ${computerChoice}. <strong>It's a tie!</strong>`
+        roundMsgNode.innerHTML = `The computer chose ${computerChoice}. <strong>It's a tie!</strong>`;
     } else {
         let userWon = didUserWinRound(userChoice, computerChoice);
 
         if (userWon) {
-            roundMsgNode.innerHTML = `The computer chose ${computerChoice}. <strong>You win!</strong>`
+            roundMsgNode.innerHTML = `The computer chose ${computerChoice}. <strong>You win!</strong>`;
 
             computerHP--;
-            computerHPContainer = document.querySelector('.computer-hp')
             for (node of computerHPContainer.children) {
                 if (node.textContent === '❤️') {
-                    node.textContent = '🖤'
-                    break
+                    node.textContent = '🖤';
+                    break;
                 }
             }
 
             if (computerHP === 0) { // if we won
-                gameOverMessage = 'You won 5 times. You win the game! Good job!'
-            }
+                gameOverMessage = 'You won 5 times. You win the game! Good job!';
+            };
         } else {
-            roundMsgNode.innerHTML = `The computer chose ${computerChoice}. <strong>You lose!</strong>`
+            roundMsgNode.innerHTML = `The computer chose ${computerChoice}. <strong>You lose!</strong>`;
 
             userHP--;
-            userHPContainer = document.querySelector('.user-hp')
             for (node of userHPContainer.children) {
                 if (node.textContent === '❤️') {
-                    node.textContent = '🖤'
-                    break
+                    node.textContent = '🖤';
+                    break;
                 } 
             }
 
             if (userHP === 0) { // if we lost
-                gameOverMessage = 'The computer won 5 times. You lose the game! Better luck next time!'
-            }
+                gameOverMessage = 'The computer won 5 times. You lose the game! Better luck next time!';
+            };
         }
 
         if (gameOverMessage) {
             setTimeout(() => {
-                document.querySelector('.modal-body-text').textContent = gameOverMessage
-                gameOverModal.style.display = 'block'
+                document.querySelector('.modal-body-text').textContent = gameOverMessage;
+                gameOverModal.style.display = 'block';
             }, 300);
         }
     }
